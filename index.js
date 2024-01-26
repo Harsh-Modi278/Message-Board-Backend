e92f326d8b03cef28f306cb7db0c2235137898df
Harsh-Modi278 <harshmodi278@gmail.com>
2024-01-26 11:43:22 +0530

    fixing cors

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 
-// cors related
-const cors = require("cors");
-
 const boardsRoutes = require("./routes/boardsRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
 const commentRoutes = require("./routes/commentRoutes.js");
@@ -14,28 +11,26 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// enable all cors requests
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+// Enable all CORS requests globally
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  );
   if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    res.status(200).end();
+    return;
   }
-  return await fn(req, res)
-}
+  next();
+});
 
-app.use("/api/boards", allowCors(boardsRoutes));
-app.use("/api/auth", allowCors(authRoutes));
-app.use("/api/comments", allowCors(commentRoutes));
-app.use("/api", allowCors(miscRoutes)); //keep it at last
+app.use("/api/boards", boardsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api", miscRoutes); //keep it at last
 
 // catch-all route
 // If request is able pass till here, route was not found. => Send 404 error

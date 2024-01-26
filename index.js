144d3ab090b447b879b22239a6dc49ea8508c094
Harsh-Modi278 <harshmodi278@gmail.com>
2024-01-26 11:32:05 +0530

    enable cors on vercel

--- index.js
+++ index.js
@@ -15,12 +15,27 @@ app.use(express.json());
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // enable all cors requests
-app.use(cors());
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
 
-app.use("/api/boards", boardsRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api", miscRoutes); //keep it at last
+app.use("/api/boards", allowCors(boardsRoutes));
+app.use("/api/auth", allowCors(authRoutes));
+app.use("/api/comments", allowCors(commentRoutes));
+app.use("/api", allowCors(miscRoutes)); //keep it at last
 
 // catch-all route
 // If request is able pass till here, route was not found. => Send 404 error
